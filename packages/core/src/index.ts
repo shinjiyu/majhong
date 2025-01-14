23a42e3fc27b83ac2f2abd1ce158f98831800310
shinjiyu <yu.zhenyu@hellogroup.com>
2025-01-14 17:07:14 +0800

    feat:启用cache

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,8 +3,9 @@ export { TilePattern } from './TilePattern';
 export { PatternSolver } from './PatternSolver';
 export { PatternSolverWithJoker } from './PatternSolverWithJoker';
 export { StandardForm } from './StandardForm';
-export { PatternParser , PatternInput} from './PatternParser';
+export { PatternParser, PatternInput } from './PatternParser';
 export { PatternPreprocessor } from './PatternPreprocessor';
+export { PatternCache } from './PatternCache';
 
 // 导出类型定义
 export type { Solution, ScoredCombination } from './types'; 
